74e6fc900738e0c4eeefdeb58133f35242c1debd
thibaultg <thibaultg78@users.noreply.github.com>
2016-03-11 23:49:47 +0100

    Update Disable-RDP.ps1

--- Post-Configuration/Disable-RDP.ps1
+++ Post-Configuration/Disable-RDP.ps1
@@ -6,7 +6,7 @@
 #-------------------------------------------------------------------------------
 function Disable-RDP() {
 	Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 1
-	Write-Host "RDP est maintenant desactive" $LogInfo
+	Write-Host "RDP est maintenant desactive"
 	# Only on Windows Server 2012 R2 to create an exception in MS Windows Firewall
 	# Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
 }
