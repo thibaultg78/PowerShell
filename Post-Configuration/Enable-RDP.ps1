c1609a1d59f34f2057f6713a8d2a79c15d7a818a
thibaultg <thibaultg78@users.noreply.github.com>
2016-03-11 23:50:11 +0100

    Update Enable-RDP.ps1

--- Post-Configuration/Enable-RDP.ps1
+++ Post-Configuration/Enable-RDP.ps1
@@ -6,7 +6,7 @@
 #-------------------------------------------------------------------------------
 function Enable-RDP() {
 	Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0
-	Write-Host "RDP est maintenant active" $LogInfo
+	Write-Host "RDP est maintenant active"
 	# Only on Windows Server 2012 R2 to create an exception in MS Windows Firewall
 	# Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True
 }
