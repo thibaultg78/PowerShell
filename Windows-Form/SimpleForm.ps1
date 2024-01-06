d7aecfc53a70293a98980e8a7c5dfd3052af7a2d
thibaultg78 <thibaultg78@users.noreply.github.com>
2024-01-07 00:49:45 +0100

    Update SimpleForm.ps1

--- Windows-Form/SimpleForm.ps1
+++ Windows-Form/SimpleForm.ps1
@@ -1,6 +1,6 @@
 #################################################
 # 2013-04-07 - TGI
-# Downloaded on http://blog.akril.net
+# Downloaded on https://akril.net
 # Free to use as you want
 #################################################
 
